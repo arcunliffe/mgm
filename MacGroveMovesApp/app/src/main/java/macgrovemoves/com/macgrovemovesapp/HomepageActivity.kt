72541e4713b493df413ea29394e54a189374bc6d
Alex Cunliffe <alexandra.cunliffe@gmail.com>
2018-09-16 14:54:04 -0500

    add logout and storage by user ID
    create menu with logout button, store trips by user ID, change app display name

--- MacGroveMovesApp/app/src/main/java/macgrovemoves/com/macgrovemovesapp/HomepageActivity.kt
+++ MacGroveMovesApp/app/src/main/java/macgrovemoves/com/macgrovemovesapp/HomepageActivity.kt
@@ -1,8 +1,11 @@
 package macgrovemoves.com.macgrovemovesapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_homepage.*
@@ -19,58 +22,50 @@ class HomepageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_homepage)
 
         Firebase.setAndroidContext(this)
-        val deviceID = Settings.Secure.getString(applicationContext.contentResolver, Settings.Secure.ANDROID_ID)
+        // val deviceID = Settings.Secure.getString(applicationContext.contentResolver, Settings.Secure.ANDROID_ID)
         val user = FirebaseAuth.getInstance().currentUser
-        val uid = ""
+
         if (user != null) {
-            val uid = user.uid
-            Toast.makeText(this, "uid " + uid, Toast.LENGTH_LONG).show()
+            val uid:String = user.uid
+            val myFirebase = Firebase("https://macgrovemoves.firebaseio.com/Users/" + uid)
+
+            // add trips buttons
+            p_walk.setOnClickListener { addTrips("walk", myFirebase) }
+            p_run.setOnClickListener { addTrips("run", myFirebase) }
+            p_bike.setOnClickListener { addTrips("bike", myFirebase) }
+            p_transit.setOnClickListener { addTrips("transit", myFirebase) }
+
+            // subtract trips buttons
+            m_walk.setOnClickListener { subtractTrips("walk", myFirebase) }
+            m_run.setOnClickListener { subtractTrips("run", myFirebase) }
+            m_bike.setOnClickListener { subtractTrips("bike", myFirebase) }
+            m_transit.setOnClickListener { subtractTrips("transit", myFirebase) }
+
+            /** Update number of trips from server */
+            updateNTripsFromServer(myFirebase)
+
         } else{
             Toast.makeText(this, "user is null", Toast.LENGTH_SHORT).show()
         }
-        //TODO: Change input to uid
-        val myFirebase = Firebase("https://macgrovemoves.firebaseio.com/Users/" + deviceID)
 
-        // add trips buttons
-        p_walk.setOnClickListener { addTrips("walk", myFirebase) }
-        p_run.setOnClickListener { addTrips("run", myFirebase) }
-        p_bike.setOnClickListener { addTrips("bike", myFirebase) }
-        p_transit.setOnClickListener { addTrips("transit", myFirebase) }
 
-        // subtract trips buttons
-        m_walk.setOnClickListener { subtractTrips("walk", myFirebase) }
-        m_run.setOnClickListener { subtractTrips("run", myFirebase) }
-        m_bike.setOnClickListener { subtractTrips("bike", myFirebase) }
-        m_transit.setOnClickListener { subtractTrips("transit", myFirebase) }
+    }
 
-        /** Update number of trips from server */
-        updateNTripsFromServer(myFirebase)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_homepage, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
-        /**
-        /** Send number of trips to server on button click */
-        button.setOnClickListener(View.OnClickListener {
-
-            // walk
-            val walkKey = myFirebase.child("walk")
-            walkKey.setValue(parseTripText(text_walktrips.text.toString()))
-
-            // run
-            val runKey = myFirebase.child("run")
-            runKey.setValue(parseTripText(text_runtrips.text.toString()))
-
-            // bike
-            val bikeKey = myFirebase.child("bike")
-            bikeKey.setValue(parseTripText(text_biketrips.text.toString()))
-
-            // transit
-            val transitKey = myFirebase.child("transit")
-            transitKey.setValue(parseTripText(text_transittrips.text.toString()))
-
-            Toast.makeText(this, "Updating trip count", Toast.LENGTH_SHORT).show()
-
-        })
-        */
-
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId){
+            R.id.logout -> {
+                val myAuth = FirebaseAuth.getInstance()
+                myAuth.signOut()
+                val intent = Intent(this, MyLoginActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     /**Parse trips string to get current number of trips */
