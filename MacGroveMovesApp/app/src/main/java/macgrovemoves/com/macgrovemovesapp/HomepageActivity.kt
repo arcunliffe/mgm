d84a9dc4b659988dfaad00b40b35fa7ed67385ee
Alex Cunliffe <alexandra.cunliffe@gmail.com>
2018-09-22 17:07:49 -0500

    medals and raffle coutdown

--- MacGroveMovesApp/app/src/main/java/macgrovemoves/com/macgrovemovesapp/HomepageActivity.kt
+++ MacGroveMovesApp/app/src/main/java/macgrovemoves/com/macgrovemovesapp/HomepageActivity.kt
@@ -1,11 +1,12 @@
 package macgrovemoves.com.macgrovemovesapp
 
 import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.Settings
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageView
 import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_homepage.*
@@ -17,6 +18,7 @@ import com.google.firebase.auth.FirebaseAuth
 
 class HomepageActivity : AppCompatActivity() {
 
+    /** main */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
@@ -41,7 +43,7 @@ class HomepageActivity : AppCompatActivity() {
             m_bike.setOnClickListener { subtractTrips("bike", myFirebase) }
             m_transit.setOnClickListener { subtractTrips("transit", myFirebase) }
 
-            /** Update number of trips from server */
+            // Update number of trips from server
             updateNTripsFromServer(myFirebase)
 
         } else{
@@ -51,11 +53,13 @@ class HomepageActivity : AppCompatActivity() {
 
     }
 
+    /** Create menu */
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_homepage, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
+    /** Menu options */
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         when (item?.itemId){
             R.id.logout -> {
@@ -64,11 +68,15 @@ class HomepageActivity : AppCompatActivity() {
                 val intent = Intent(this, MyLoginActivity::class.java)
                 startActivity(intent)
             }
+            R.id.about -> {
+                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://macgrove.org/what-we-do/mgm/"))
+                startActivity(browserIntent)
+            }
         }
         return super.onOptionsItemSelected(item)
     }
 
-    /**Parse trips string to get current number of trips */
+    /** Parse trips string to get current number of trips */
     private fun parseTripText(text: String): Int {
         val separateWords = text.split(" ".toRegex())
         return separateWords[0].toInt()
@@ -83,6 +91,66 @@ class HomepageActivity : AppCompatActivity() {
         return newTripsString
     }
 
+    private fun countTotalTrips(): Int{
+        val totalTrips = parseTripText(text_walktrips.text.toString()) + parseTripText(text_runtrips.text.toString()) + parseTripText(text_transittrips.text.toString()) + parseTripText(text_biketrips.text.toString())
+        return totalTrips
+    }
+
+    /** Make cumulative trips text */
+    private fun makeCumulativeTripText() {
+        val totalTrips = countTotalTrips()
+        if (totalTrips < 15) {
+            val remainingTrips = 15 - totalTrips
+            cum_trips.text = "Trips until next raffle entry: "  + remainingTrips.toString()
+        }
+        else if (totalTrips < 30){
+            val remainingTrips = 30 - totalTrips
+            cum_trips.text = "Trips until next raffle entry: "  + remainingTrips.toString()
+        }
+        else if (totalTrips < 45) {
+            val remainingTrips = 45 - totalTrips
+            cum_trips.text = "Trips until next raffle entry: "  + remainingTrips.toString()
+        }
+        else {
+            cum_trips.text = "Challenge completed. Congratulations!"
+        }
+        updateMedal(totalTrips)
+    }
+
+    /** Update medal */
+    private fun updateMedal(totalTrips: Int){
+        val goldImage = findViewById<ImageView>(R.id.gold)
+        val silverImage = findViewById<ImageView>(R.id.silver)
+        val bronzeImage = findViewById<ImageView>(R.id.bronze)
+        if (totalTrips >= 45){
+            // all medals
+            goldImage.setImageResource(R.drawable.gold_64_t)
+            silverImage.setImageResource(R.drawable.silver_64_t)
+            bronzeImage.setImageResource(R.drawable.bronze_64_t)
+        }
+        else if (totalTrips >= 30){
+            // silver & bronze medals
+            goldImage.setImageResource(R.drawable.gray_64)
+            silverImage.setImageResource(R.drawable.silver_64_t)
+            bronzeImage.setImageResource(R.drawable.bronze_64_t)
+        }
+        else if (totalTrips >= 15){
+            // bronze medal
+            goldImage.setImageResource(R.drawable.gray_64)
+            silverImage.setImageResource(R.drawable.gray_64)
+            bronzeImage.setImageResource(R.drawable.bronze_64_t)
+        }
+        else {
+            // no medals
+            goldImage.setImageResource(R.drawable.gray_64)
+            silverImage.setImageResource(R.drawable.gray_64)
+            bronzeImage.setImageResource(R.drawable.gray_64)
+        }
+
+    }
+
+    /** Pop-up to submit raffle entry */
+
     /** Get number of trips from server and update on app */
     private fun updateNTripsFromServer(myFirebase: Firebase){
         myFirebase.addListenerForSingleValueEvent(object: ValueEventListener{
@@ -93,6 +161,13 @@ class HomepageActivity : AppCompatActivity() {
                     text_runtrips.text = makeTripText(map["run"].toString())
                     text_biketrips.text = makeTripText(map["bike"].toString())
                     text_transittrips.text = makeTripText(map["transit"].toString())
+
+                    // display total trips left
+                    makeCumulativeTripText()
+
+                    // set up medal image
+                    val totalTrips = countTotalTrips()
+                    updateMedal(totalTrips)
                 }
             }
 
@@ -133,14 +208,16 @@ class HomepageActivity : AppCompatActivity() {
 
         }
 
+        // total
+        makeCumulativeTripText()
+
         // update value on server
         if (nTrips != -1){
             val activityKey = myFirebase.child(activity_type)
             activityKey.setValue(nTrips.toString())
-            Toast.makeText(this, "Updating trip count", Toast.LENGTH_SHORT).show()
+            // Toast.makeText(this, "Updating trip count", Toast.LENGTH_SHORT).show()
         }
     }
-
 
     /**Update app text to subtract trips */
     private fun subtractTrips(activity_type: String, myFirebase: Firebase){
@@ -190,12 +267,15 @@ class HomepageActivity : AppCompatActivity() {
             }
         }
 
+        // total
+        makeCumulativeTripText()
+
         // update value on server
         if (updateServerVal) {
             // update value on server
             val activityKey = myFirebase.child(activity_type)
             activityKey.setValue(nTrips.toString())
-            Toast.makeText(this, "Updating trip count", Toast.LENGTH_SHORT).show()
+            // Toast.makeText(this, "Updating trip count", Toast.LENGTH_SHORT).show()
         }
     }
 
